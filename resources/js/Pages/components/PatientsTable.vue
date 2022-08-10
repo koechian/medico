49c2aedf761582ddfcd6a0278ea713e8952a54c2
koechian <ianovic8@live.com>
2022-08-10 10:33:00 +0300

    new patient

--- resources/js/Pages/components/PatientsTable.vue
+++ resources/js/Pages/components/PatientsTable.vue
@@ -6,33 +6,52 @@
                 <th>Name</th>
                 <th>Current Assigned Dept</th>
                 <th>Current Assigned Doctor</th>
+                <th><button v-on:click="getPatients()">Refresh</button></th>
             </tr>
         </thead>
-        <tbody>
-            <td>{{ id }}</td>
-            <td>{{ name }}</td>
-            <td>{{ dept }}</td>
-            <td>{{ doctor }}</td>
+        <tbody v-for="patient in patients">
+            <td>{{ patient.id }}</td>
+            <td>{{ patient.name }}</td>
+            <td>{{ patient.dept }}</td>
+            <!-- <td>{{ doctor }}</td> -->
         </tbody>
     </table>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "PatientsTable",
     data() {
         return {
-            id: "",
-            name: "",
-            dept: "",
-            doctor: "",
+            patients: [
+                {
+                    id: "",
+                    name: "",
+                    dept: "",
+                    // doctor: "",
+                },
+            ],
         };
     },
-    methods: {},
+    methods: {
+        getPatients() {
+            axios.get("api/data/getPatients").then((response) => {
+                response.data.forEach((element) => {
+                    this.patients.id = element;
+                });
+                this.patients.id = response.data.id;
+                this.patients.name = patients.firstname + patients.lastname;
+                this.patients.dept = patients.dept;
+            });
+        },
+    },
 };
 </script>
 <style>
 table {
+    z-index: 1;
     width: 100%;
     padding-top: 20px;
 }
